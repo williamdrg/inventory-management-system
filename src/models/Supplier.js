23b0ef5756c748382ce498d16887508b03501c0c
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-08-16 14:09:36 -0500

    creación del modelo supplier

--- src/models/Supplier.js
+++ src/models/Supplier.js
@@ -2,24 +2,29 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection');
 
 const Supplier = sequelize.define('supplier', {
-    name: {
-        type: DataTypes.STRING(30),
-        allowNull: false
-    },
-    dni: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-    },
-    phone: {
-        type: DataTypes.STRING(30),
-    },
-    address: {
-        type: DataTypes.STRING(100),
-    },
+  name: {
+    type: DataTypes.STRING(30),
+    allowNull: false
+  },
+  dni: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  phone: {
+    type: DataTypes.STRING(30),
+  },
+  address: {
+    type: DataTypes.STRING(100),
+  },
 });
 
 module.exports = Supplier;
